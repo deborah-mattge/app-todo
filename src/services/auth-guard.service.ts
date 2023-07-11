6063343b08dbae7f7c5c79684cf967270cafddc7
deborah-mattge <elomattge@gmail.com>
2023-07-10 21:51:11 -0300

    inicio tabelas auxiliares

--- src/services/auth-guard.service.ts
+++ src/services/auth-guard.service.ts
@@ -4,11 +4,10 @@ import { Observable } from "rxjs";
 @Injectable()
     export class AuthGuardService implements CanActivate{
         canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-            if(document.cookie===null){
-                window.location.replace('/login')
+    
+                // window.location.replace('/login')
                 return false;
 
-            }
               
                 
             
